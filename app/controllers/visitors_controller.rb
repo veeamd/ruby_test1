9c56e1bd692abd93dc25627a784c13f773b9689b
Wei Zhang <veeamd@gmail.com>
2016-05-30 20:46:37 -0400

    add better_errors gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,9 @@
 class VisitorsController < ApplicationController
 
   def new
+    logger.debug "DEBUG:  entering 'new' method"
     @owner = Owner.new
+    logger.debug "DEBUG: owner name is " + @owner.name
   end
 
 end
