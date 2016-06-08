d272157966732ace79eb6348929b41e7ec3eef88
Wei Zhang <veeamd@gmail.com>
2016-06-07 23:10:19 -0400

    contact form

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,8 +4,6 @@ class VisitorsController < ApplicationController
     logger.debug "DEBUG:  entering 'new' method"
     @owner = Owner.new
     logger.debug "DEBUG: owner name is " + @owner.name
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
